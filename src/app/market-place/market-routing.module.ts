1c0a5e42ab3b53744b1602c6d4673f812025ddff
munene brian <njerubrianmunene@gmail.com>
2023-03-21 18:58:29 +0300

    feat: done working on market page, start details

--- src/app/market-place/market-routing.module.ts
+++ src/app/market-place/market-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeMarketComponent } from './home-market/home-market.component';
+import { MarketDetailsComponent } from './market-details/market-details.component';
 import { MarketPlaceComponent } from './market-place.component';
 const routes: Routes = [
   {
@@ -8,6 +9,7 @@ const routes: Routes = [
     component: MarketPlaceComponent ,
     children: [
         {path:"",component: HomeMarketComponent},
+        {path:"market-details/:id",component: MarketDetailsComponent},
       ],
   }
 ];
