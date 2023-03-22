a8e666c8acbd9da4bbfe6ce2a99bc8a1d0053b17
Salome <mbulambithe@gmail.com>
2023-03-22 13:23:16 +0300

    feat: meet the team

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HowItWorksComponent,
   ],
   imports: [
     BrowserModule,
